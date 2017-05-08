4e00df5e47034bdc840c2bb27c10028a650f3328
ohyesnoyes <brothernoyes@gmail.com>
2017-05-07 19:34:14 -0600

    add email form to jumbotron and content to home. fix content on portfolio

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -6,6 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  isIn = false; //store state
+  
+  toggleState() { //click hander
+    let bool = this.isIn;
+    this.isIn = bool === false ? true : false;
+  }
 
   constructor() { }
 
